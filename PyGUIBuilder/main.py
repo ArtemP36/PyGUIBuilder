f7d0dd5b510b6288b369f4702cb8a371f7a47a68
Artem <118052032+ArtemP36@users.noreply.github.com>
2024-06-26 13:40:25 +0300

    Update main.py
    Update to 1.1

--- PyGUIBuilder/main.py
+++ PyGUIBuilder/main.py
@@ -36,6 +36,24 @@ mygui_dll.getText_dll.restype = c_char_p
 mygui_dll.setText_dll.argtypes = [HWND, c_char_p]
 mygui_dll.setText_dll.restype = None
 
+mygui_dll.showMessageBox_dll.argtypes = [c_char_p, c_char_p, c_char_p]
+mygui_dll.showMessageBox_dll.restype = c_int
+
+mygui_dll.createComboBox_dll.argtypes = [Window_t, POINTER(c_char_p), c_int, c_int, c_int]
+mygui_dll.createComboBox_dll.restype = c_void_p
+
+mygui_dll.getComboBoxSelection_dll.argtypes = [c_void_p]
+mygui_dll.getComboBoxSelection_dll.restype = c_char_p
+
+# Define MessageBox return values
+IDOK = 1
+IDCANCEL = 2
+IDABORT = 3
+IDRETRY = 4
+IDIGNORE = 5
+IDYES = 6
+IDNO = 7
+
 def createWindow(title, icon, width, height):
     return mygui_dll.createWindow_dll(title.encode('utf-8'), icon.encode('utf-8'), width, height)
 
@@ -66,6 +84,34 @@ def setText(hwnd, text):
 
 def destroyElement(hwnd):
     ctypes.windll.user32.DestroyWindow(hwnd)
+
+def showMessageBox(type, title, message):
+    
+    result = mygui_dll.showMessageBox_dll(type.encode('utf-8'), title.encode('utf-8'), message.encode('utf-8'))
+    
+    if result == IDOK:
+        return "OK"
+    elif result == IDCANCEL:
+        return "Cancel"
+    elif result == IDYES:
+        return "Yes"
+    elif result == IDNO:
+        return "No"
+    elif result == IDRETRY:
+        return "Retry"
+    elif result == IDABORT:
+        return "Abort"
+    elif result == IDIGNORE:
+        return "Ignore"
+    else:
+        return "Unknown"
+
+def createComboBox(window, options, row, column):
+    options_c = (c_char_p * len(options))(*[opt.encode('utf-8') for opt in options])
+    return mygui_dll.createComboBox_dll(window, options_c, len(options), row, column)
+
+def getComboBoxSelection(comboBox):
+    return mygui_dll.getComboBoxSelection_dll(comboBox).decode('utf-8')
 
 def run():
     msg = ctypes.wintypes.MSG()
