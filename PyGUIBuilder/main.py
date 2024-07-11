dbfb3f8c6439406e07934615d22cbb448d8c471c
Artem <118052032+ArtemP36@users.noreply.github.com>
2024-07-11 13:00:04 +0300

    Update to 1.2 main.py

--- PyGUIBuilder/main.py
+++ PyGUIBuilder/main.py
@@ -5,7 +5,7 @@ import os
 
 dll_path = os.path.join(os.path.dirname(__file__), 'PyGUIBuilder.dll')
 
-mygui_dll = ctypes.CDLL(dll_path)
+g = ctypes.CDLL(dll_path)
 
 HWND = c_int
 
@@ -15,35 +15,39 @@ class Window_t(Structure):
                 ("width", c_int),
                 ("height", c_int)]
 
-mygui_dll.createWindow_dll.argtypes = [c_char_p, c_char_p, c_int, c_int]
-mygui_dll.createWindow_dll.restype = Window_t
+class Checkbox_t(Structure):
+    _fields_ = [("hwnd", c_void_p),
+                ("checked", c_bool)]
 
-mygui_dll.createLabel_dll.argtypes = [Window_t, c_char_p, c_int, c_int]
-mygui_dll.createLabel_dll.restype = c_void_p
+g.createWindow_dll.argtypes = [c_char_p, c_char_p, c_int, c_int]
+g.createWindow_dll.restype = Window_t
 
-mygui_dll.createButton_dll.argtypes = [Window_t, c_char_p, CFUNCTYPE(None), c_int, c_int]
-mygui_dll.createButton_dll.restype = c_void_p
+g.createLabel_dll.argtypes = [Window_t, c_char_p, c_int, c_int]
+g.createLabel_dll.restype = c_void_p
 
-mygui_dll.createEntry_dll.argtypes = [Window_t, c_char_p, c_int, c_int]
-mygui_dll.createEntry_dll.restype = c_void_p
+g.createButton_dll.argtypes = [Window_t, c_char_p, CFUNCTYPE(None), c_int, c_int]
+g.createButton_dll.restype = c_void_p
 
-mygui_dll.clearText_dll.argtypes = [HWND]
-mygui_dll.clearText_dll.restype = None
+g.createEntry_dll.argtypes = [Window_t, c_char_p, c_int, c_int]
+g.createEntry_dll.restype = c_void_p
 
-mygui_dll.getText_dll.argtypes = [HWND]
-mygui_dll.getText_dll.restype = c_char_p
+g.clearText_dll.argtypes = [HWND]
+g.clearText_dll.restype = None
 
-mygui_dll.setText_dll.argtypes = [HWND, c_char_p]
-mygui_dll.setText_dll.restype = None
+g.getText_dll.argtypes = [HWND]
+g.getText_dll.restype = c_char_p
 
-mygui_dll.showMessageBox_dll.argtypes = [c_char_p, c_char_p, c_char_p]
-mygui_dll.showMessageBox_dll.restype = c_int
+g.setText_dll.argtypes = [HWND, c_char_p]
+g.setText_dll.restype = None
 
-mygui_dll.createComboBox_dll.argtypes = [Window_t, POINTER(c_char_p), c_int, c_int, c_int]
-mygui_dll.createComboBox_dll.restype = c_void_p
+g.showMessageBox_dll.argtypes = [c_char_p, c_char_p, c_char_p]
+g.showMessageBox_dll.restype = c_int
 
-mygui_dll.getComboBoxSelection_dll.argtypes = [c_void_p]
-mygui_dll.getComboBoxSelection_dll.restype = c_char_p
+g.createComboBox_dll.argtypes = [Window_t, POINTER(c_char_p), c_int, c_int, c_int]
+g.createComboBox_dll.restype = c_void_p
+
+g.getComboBoxSelection_dll.argtypes = [c_void_p]
+g.getComboBoxSelection_dll.restype = c_char_p
 
 # Define MessageBox return values
 IDOK = 1
@@ -55,10 +59,10 @@ IDYES = 6
 IDNO = 7
 
 def createWindow(title, icon, width, height):
-    return mygui_dll.createWindow_dll(title.encode('utf-8'), icon.encode('utf-8'), width, height)
+    return g.createWindow_dll(title.encode('utf-8'), icon.encode('utf-8'), width, height)
 
 def createLabel(window, text, row, column):
-    return mygui_dll.createLabel_dll(window, text.encode('utf-8'), row, column)
+    return g.createLabel_dll(window, text.encode('utf-8'), row, column)
 
 ButtonCallback = CFUNCTYPE(None)
 
@@ -67,27 +71,27 @@ _global_callbacks = []
 def createButton(window, text, callback, row, column):
     callback_c = ButtonCallback(callback)
     _global_callbacks.append(callback_c)
-    return mygui_dll.createButton_dll(window, text.encode('utf-8'), callback_c, row, column)
+    return g.createButton_dll(window, text.encode('utf-8'), callback_c, row, column)
 
 def createEntry(window, text, row, column):
-    return mygui_dll.createEntry_dll(window, text.encode('utf-8'), row, column)
+    return g.createEntry_dll(window, text.encode('utf-8'), row, column)
 
 def clearText(hwnd):
-    return mygui_dll.clearText_dll(hwnd)
+    return g.clearText_dll(hwnd)
 
 def getText(hwnd):
-    text = mygui_dll.getText_dll(hwnd)
+    text = g.getText_dll(hwnd)
     return text.decode('utf-8')
 
 def setText(hwnd, text):
-    mygui_dll.setText_dll(hwnd, text.encode('utf-8'))
+    g.setText_dll(hwnd, text.encode('utf-8'))
 
 def destroyElement(hwnd):
     ctypes.windll.user32.DestroyWindow(hwnd)
 
 def showMessageBox(type, title, message):
     
-    result = mygui_dll.showMessageBox_dll(type.encode('utf-8'), title.encode('utf-8'), message.encode('utf-8'))
+    result = g.showMessageBox_dll(type.encode('utf-8'), title.encode('utf-8'), message.encode('utf-8'))
     
     if result == IDOK:
         return "OK"
@@ -108,10 +112,13 @@ def showMessageBox(type, title, message):
 
 def createComboBox(window, options, row, column):
     options_c = (c_char_p * len(options))(*[opt.encode('utf-8') for opt in options])
-    return mygui_dll.createComboBox_dll(window, options_c, len(options), row, column)
+    return g.createComboBox_dll(window, options_c, len(options), row, column)
 
 def getComboBoxSelection(comboBox):
-    return mygui_dll.getComboBoxSelection_dll(comboBox).decode('utf-8')
+    return g.getComboBoxSelection_dll(comboBox).decode('utf-8')
+
+def createCheckbox(window, text, row, column):
+    return g.createCheckbox_dll(window, text.encode('utf-8'), row, column)
 
 def run():
     msg = ctypes.wintypes.MSG()
