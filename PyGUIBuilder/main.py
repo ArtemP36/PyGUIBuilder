e0f067943083840ff5a12f9e5c602f00317b9ffe
Artem <118052032+ArtemP36@users.noreply.github.com>
2024-07-18 16:55:32 +0300

    Update to 1.3 main.py

--- PyGUIBuilder/main.py
+++ PyGUIBuilder/main.py
@@ -15,10 +15,6 @@ class Window_t(Structure):
                 ("width", c_int),
                 ("height", c_int)]
 
-class Checkbox_t(Structure):
-    _fields_ = [("hwnd", c_void_p),
-                ("checked", c_bool)]
-
 g.createWindow_dll.argtypes = [c_char_p, c_char_p, c_int, c_int]
 g.createWindow_dll.restype = Window_t
 
@@ -49,7 +45,16 @@ g.createComboBox_dll.restype = c_void_p
 g.getComboBoxSelection_dll.argtypes = [c_void_p]
 g.getComboBoxSelection_dll.restype = c_char_p
 
-# Define MessageBox return values
+g.createCheckbox_dll.argtypes = [Window_t, c_char_p, c_int, c_int]
+g.createCheckbox_dll.restype = c_void_p
+
+g.getCheckbox_dll.argtypes = [HWND]
+g.getCheckbox_dll.restype = c_void_p
+
+ButtonCallback = CFUNCTYPE(None)
+
+_global_callbacks = []
+
 IDOK = 1
 IDCANCEL = 2
 IDABORT = 3
@@ -63,10 +68,6 @@ def createWindow(title, icon, width, height):
 
 def createLabel(window, text, row, column):
     return g.createLabel_dll(window, text.encode('utf-8'), row, column)
-
-ButtonCallback = CFUNCTYPE(None)
-
-_global_callbacks = []
 
 def createButton(window, text, callback, row, column):
     callback_c = ButtonCallback(callback)
@@ -90,7 +91,6 @@ def destroyElement(hwnd):
     ctypes.windll.user32.DestroyWindow(hwnd)
 
 def showMessageBox(type, title, message):
-    
     result = g.showMessageBox_dll(type.encode('utf-8'), title.encode('utf-8'), message.encode('utf-8'))
     
     if result == IDOK:
@@ -119,6 +119,9 @@ def getComboBoxSelection(comboBox):
 
 def createCheckbox(window, text, row, column):
     return g.createCheckbox_dll(window, text.encode('utf-8'), row, column)
+
+def getCheckbox(hwnd):
+    return g.getCheckbox_dll(hwnd)
 
 def run():
     msg = ctypes.wintypes.MSG()
